023ff0eebc9c349cdfe160425cdf974fb4aa0def
haomengjie <qqduxingzhe@163.com>
2017-05-31 09:22:56 +0800

    init

---
+++ app/src/main/java/com/dzet/learnandroiddemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dzet.learnandroiddemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
