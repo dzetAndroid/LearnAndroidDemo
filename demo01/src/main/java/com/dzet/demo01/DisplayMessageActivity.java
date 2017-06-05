b4a432ed09a710343ba33e7f4dc842813bd6c88d
haomengjie <qqduxingzhe@163.com>
2017-06-05 11:52:33 +0800

    add demo02

--- demo01/src/main/java/com/dzet/demo01/DisplayMessageActivity.java
+++ demo01/src/main/java/com/dzet/demo01/DisplayMessageActivity.java
@@ -3,11 +3,17 @@ package com.dzet.demo01;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    private static final String TAG = "DisplayMessageActivity";
+
+    /**
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +27,17 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
+
+        // 获取一个字符串   从资源目录中
+        // 创建多个语言区域目录的好处
+        // values   values-zh
+        //
+        // 显示字符串资源时，会根据当前手机本地语言做匹配
+        // 就是说, 我当前的module提供了英文版和中文版
+
+        // 那么，我在进行Log输出时，显示的就是中文
+        String hello = getResources().getString(R.string.hello_world);
+        Log.d(TAG, "onCreate: " + hello);
+
     }
 }
